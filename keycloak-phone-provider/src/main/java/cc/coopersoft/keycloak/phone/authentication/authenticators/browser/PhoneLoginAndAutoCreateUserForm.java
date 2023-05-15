705926d1a47ec89372c1591921c2a8d268029cac
LAPTOP-MJEE01LB\nguye <nguyenquocvuong23061999@gmail.com>
2023-05-15 14:20:11 +0700

    TV75 Add module keycloak: create form login and auto create user

---
+++ keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/authentication/authenticators/browser/PhoneLoginAndAutoCreateUserForm.java
@@ -0,0 +1,154 @@
+package cc.coopersoft.keycloak.phone.authentication.authenticators.browser;
+
+import cc.coopersoft.keycloak.phone.Utils;
+import cc.coopersoft.keycloak.phone.providers.exception.PhoneNumberInvalidException;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
+import org.keycloak.authentication.authenticators.browser.AbstractUsernameFormAuthenticator;
+import org.keycloak.authentication.authenticators.browser.UsernameForm;
+import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
+import org.keycloak.events.Details;
+import org.keycloak.events.Errors;
+import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.ModelDuplicateException;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.utils.KeycloakModelUtils;
+import org.keycloak.services.ServicesLogger;
+import org.keycloak.services.managers.AuthenticationManager;
+import org.keycloak.services.messages.Messages;
+import org.keycloak.userprofile.UserProfile;
+import org.keycloak.userprofile.UserProfileContext;
+import org.keycloak.userprofile.UserProfileProvider;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
+
+import static org.keycloak.services.validation.Validation.FIELD_USERNAME;
+
+public class PhoneForm extends UsernamePasswordForm {
+    private static final String MOVITEL_DIALING_CODE = "258";
+
+    private UsernameForm usernameForm = new UsernameForm();
+
+    @Override
+    public void authenticate(AuthenticationFlowContext context) {
+        usernameForm.authenticate(context);
+    }
+
+    @Override
+    protected boolean validateForm(AuthenticationFlowContext context, MultivaluedMap<String, String> formData) {
+        UserModel user = getUser(context, formData);
+        return user != null && validateUser(context, user, formData);
+    }
+
+    private UserModel getUser(AuthenticationFlowContext context, MultivaluedMap<String, String> inputData) {
+        if (isUserAlreadySetBeforeUsernamePasswordAuth(context)) {
+            // Get user from the authentication context in case he was already set before this authenticator
+            UserModel user = context.getUser();
+            testInvalidUser(context, user);
+            return user;
+        } else {
+            // Normal login. In this case this authenticator is supposed to establish identity of the user from the provided username
+            context.clearUser();
+            return getUserFromForm(context, inputData);
+        }
+    }
+
+    private UserModel getUserFromForm(AuthenticationFlowContext context, MultivaluedMap<String, String> inputData) {
+        String username = inputData.getFirst(AuthenticationManager.FORM_USERNAME);
+        if (username == null) {
+            context.getEvent().error(Errors.USER_NOT_FOUND);
+            Response challengeResponse = challenge(context, getDefaultChallengeMessage(context), FIELD_USERNAME);
+            context.failureChallenge(AuthenticationFlowError.INVALID_USER, challengeResponse);
+            return null;
+        }
+        username = standardizePhoneNumber(username);
+        try {
+            Utils.canonicalizePhoneNumber(context.getSession(), username);
+        } catch (PhoneNumberInvalidException e) {
+            context.getEvent().error(Errors.INVALID_INPUT);
+            Response challengeResponse = challenge(context, e.getErrorType().message(), FIELD_USERNAME);
+            context.failureChallenge(AuthenticationFlowError.INVALID_USER, challengeResponse);
+            return null;
+        }
+        context.getEvent().detail(Details.USERNAME, username);
+        context.getAuthenticationSession().setAuthNote(AbstractUsernameFormAuthenticator.ATTEMPTED_USERNAME, username);
+
+        UserModel user = null;
+        try {
+            user = KeycloakModelUtils.findUserByNameOrEmail(context.getSession(), context.getRealm(), username);
+
+        } catch (ModelDuplicateException mde) {
+            ServicesLogger.LOGGER.modelDuplicateException(mde);
+
+            // Could happen during federation import
+            if (mde.getDuplicateFieldName() != null && mde.getDuplicateFieldName().equals(UserModel.EMAIL)) {
+                setDuplicateUserChallenge(context, Errors.EMAIL_IN_USE, Messages.EMAIL_EXISTS, AuthenticationFlowError.INVALID_USER);
+            } else {
+                setDuplicateUserChallenge(context, Errors.USERNAME_IN_USE, Messages.USERNAME_EXISTS, AuthenticationFlowError.INVALID_USER);
+            }
+            return user;
+        }
+
+        if (user == null) {
+            inputData.put(Details.USERNAME, Arrays.asList(username));
+            KeycloakSession session = context.getSession();
+            UserProfileProvider profileProvider = session.getProvider(UserProfileProvider.class);
+            UserProfile profile = profileProvider.create(UserProfileContext.REGISTRATION_USER_CREATION, inputData);
+            user = profile.create();
+            user.setEnabled(true);
+            user.setAttribute("phoneNumber", Arrays.asList(username));
+            user.setAttribute("phoneNumberVerified", Arrays.asList("true"));
+        }
+        return user;
+    }
+
+    public String standardizePhoneNumber(String phoneNumber) {
+        phoneNumber = phoneNumber.trim();
+        if (phoneNumber.startsWith(MOVITEL_DIALING_CODE)) {
+            return "+" + phoneNumber;
+        } else {
+            return "+" + MOVITEL_DIALING_CODE + phoneNumber.substring(1);
+        }
+    }
+
+    private boolean validateUser(AuthenticationFlowContext context, UserModel user, MultivaluedMap<String, String> inputData) {
+        if (!enabledUser(context, user)) {
+            return false;
+        }
+        String rememberMe = inputData.getFirst("rememberMe");
+        boolean remember = rememberMe != null && rememberMe.equalsIgnoreCase("on");
+        if (remember) {
+
+            context.getAuthenticationSession().setAuthNote(Details.REMEMBER_ME, "true");
+            context.getEvent().detail(Details.REMEMBER_ME, "true");
+        } else {
+            context.getAuthenticationSession().removeAuthNote(Details.REMEMBER_ME);
+        }
+        context.setUser(user);
+        return true;
+    }
+
+    @Override
+    protected Response challenge(AuthenticationFlowContext context, MultivaluedMap<String, String> formData) {
+        LoginFormsProvider forms = context.form();
+
+        if (!formData.isEmpty()) forms.setFormData(formData);
+
+        return forms.createLoginUsername();
+    }
+
+    @Override
+    protected Response createLoginForm(LoginFormsProvider form) {
+        return form.createLoginUsername();
+    }
+
+    @Override
+    protected String getDefaultChallengeMessage(AuthenticationFlowContext context) {
+        if (context.getRealm().isLoginWithEmailAllowed())
+            return Messages.INVALID_USERNAME_OR_EMAIL;
+        return Messages.INVALID_USERNAME;
+    }
+}
