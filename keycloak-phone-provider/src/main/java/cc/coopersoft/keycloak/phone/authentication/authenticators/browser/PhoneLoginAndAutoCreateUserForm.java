ff754dabfe98549753fb90cc7b048ff75ca27405
LAPTOP-MJEE01LB\nguye <nguyenquocvuong23061999@gmail.com>
2023-05-17 23:59:22 +0700

    TV75 Add module keycloak: fix comment, add credential

--- keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/authentication/authenticators/browser/PhoneLoginAndAutoCreateUserForm.java
+++ keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/authentication/authenticators/browser/PhoneLoginAndAutoCreateUserForm.java
@@ -2,12 +2,16 @@ package cc.coopersoft.keycloak.phone.authentication.authenticators.browser;
 
 import cc.coopersoft.keycloak.phone.CountryCodes;
 import cc.coopersoft.keycloak.phone.Utils;
+import cc.coopersoft.keycloak.phone.authentication.forms.SupportPhonePages;
+import cc.coopersoft.keycloak.phone.credential.PhoneOtpCredentialModel;
+import cc.coopersoft.keycloak.phone.credential.PhoneOtpCredentialProvider;
+import cc.coopersoft.keycloak.phone.credential.PhoneOtpCredentialProviderFactory;
 import cc.coopersoft.keycloak.phone.providers.exception.PhoneNumberInvalidException;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.authenticators.browser.AbstractUsernameFormAuthenticator;
-import org.keycloak.authentication.authenticators.browser.UsernameForm;
 import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
+import org.keycloak.credential.CredentialProvider;
 import org.keycloak.events.Details;
 import org.keycloak.events.Errors;
 import org.keycloak.forms.login.LoginFormsProvider;
@@ -16,33 +20,38 @@ import org.keycloak.models.ModelDuplicateException;
 import org.keycloak.models.UserModel;
 import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.services.ServicesLogger;
-import org.keycloak.services.managers.AuthenticationManager;
 import org.keycloak.services.messages.Messages;
+import org.keycloak.services.validation.Validation;
 import org.keycloak.userprofile.UserProfile;
 import org.keycloak.userprofile.UserProfileContext;
 import org.keycloak.userprofile.UserProfileProvider;
 
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import java.util.Arrays;
+import java.net.URI;
+import java.util.Collections;
 
-import static org.keycloak.services.validation.Validation.FIELD_USERNAME;
 
 public class PhoneLoginAndAutoCreateUserForm extends UsernamePasswordForm {
-
-    private UsernameForm usernameForm = new UsernameForm();
-
-    @Override
-    public void authenticate(AuthenticationFlowContext context) {
-        usernameForm.authenticate(context);
+    private void createCredential(AuthenticationFlowContext context, String username, UserModel user) {
+        PhoneOtpCredentialProvider ocp = (PhoneOtpCredentialProvider) context.getSession()
+                .getProvider(CredentialProvider.class, PhoneOtpCredentialProviderFactory.PROVIDER_ID);
+        String code = context.getHttpRequest().getDecodedFormParameters().getFirst(SupportPhonePages.FIELD_VERIFICATION_CODE);
+        ocp.createCredential(context.getRealm(), user,
+                PhoneOtpCredentialModel.create(username, code, Utils.getOtpExpires(context.getSession())));
     }
 
+    // Copy from PhoneUsernamePasswordForm
     @Override
     protected boolean validateForm(AuthenticationFlowContext context, MultivaluedMap<String, String> formData) {
         UserModel user = getUser(context, formData);
-        return user != null && validateUser(context, user, formData);
+        if (user == null) {
+            user = createUserModel(formData, context);
+        }
+        return new PhoneUsernamePasswordForm().validateUser(context, user, formData);
     }
 
+    // Copy from PhoneUsernamePasswordForm
     private UserModel getUser(AuthenticationFlowContext context, MultivaluedMap<String, String> inputData) {
         if (isUserAlreadySetBeforeUsernamePasswordAuth(context)) {
             // Get user from the authentication context in case he was already set before this authenticator
@@ -56,20 +65,22 @@ public class PhoneLoginAndAutoCreateUserForm extends UsernamePasswordForm {
         }
     }
 
+    // Copy from PhoneUsernamePasswordForm (custom validate phone Number and create user)
     private UserModel getUserFromForm(AuthenticationFlowContext context, MultivaluedMap<String, String> inputData) {
-        String username = inputData.getFirst(AuthenticationManager.FORM_USERNAME);
+        String username = inputData.getFirst(Details.USERNAME);
+        KeycloakSession session = context.getSession();
         if (username == null) {
             context.getEvent().error(Errors.USER_NOT_FOUND);
-            Response challengeResponse = challenge(context, getDefaultChallengeMessage(context), FIELD_USERNAME);
+            Response challengeResponse = challenge(context, getDefaultChallengeMessage(context), Validation.FIELD_USERNAME);
             context.failureChallenge(AuthenticationFlowError.INVALID_USER, challengeResponse);
             return null;
         }
         try {
-            Utils.canonicalizePhoneNumber(context.getSession(), username);
-            username = standardizePhoneNumber(context.getSession(), username);
+            Utils.canonicalizePhoneNumber(session, username);
+            username = standardizePhoneNumber(session, username);
         } catch (PhoneNumberInvalidException e) {
             context.getEvent().error(Errors.INVALID_INPUT);
-            Response challengeResponse = challenge(context, e.getErrorType().message(), FIELD_USERNAME);
+            Response challengeResponse = challenge(context, e.getErrorType().message(), Validation.FIELD_USERNAME);
             context.failureChallenge(AuthenticationFlowError.INVALID_USER, challengeResponse);
             return null;
         }
@@ -78,10 +89,9 @@ public class PhoneLoginAndAutoCreateUserForm extends UsernamePasswordForm {
 
         UserModel user = null;
         try {
-            user = KeycloakModelUtils.findUserByNameOrEmail(context.getSession(), context.getRealm(), username);
+            user = KeycloakModelUtils.findUserByNameOrEmail(session, context.getRealm(), username);
         } catch (ModelDuplicateException mde) {
             ServicesLogger.LOGGER.modelDuplicateException(mde);
-
             // Could happen during federation import
             if (mde.getDuplicateFieldName() != null && mde.getDuplicateFieldName().equals(UserModel.EMAIL)) {
                 setDuplicateUserChallenge(context, Errors.EMAIL_IN_USE, Messages.EMAIL_EXISTS, AuthenticationFlowError.INVALID_USER);
@@ -90,56 +100,59 @@ public class PhoneLoginAndAutoCreateUserForm extends UsernamePasswordForm {
             }
             return user;
         }
-
-        if (user == null) {
-            inputData.put(Details.USERNAME, Arrays.asList(username));
-            KeycloakSession session = context.getSession();
-            UserProfileProvider profileProvider = session.getProvider(UserProfileProvider.class);
-            UserProfile profile = profileProvider.create(UserProfileContext.REGISTRATION_USER_CREATION, inputData);
-            user = profile.create();
-            user.setEnabled(true);
-            user.setAttribute("phoneNumber", Arrays.asList(username));
-            user.setAttribute("phoneNumberVerified", Arrays.asList("true"));
-        }
         return user;
     }
 
-    public String standardizePhoneNumber(KeycloakSession session, String phoneNumber) {
+    private UserModel createUserModel(MultivaluedMap<String, String> inputData, AuthenticationFlowContext context) {
+        String username = inputData.getFirst(Details.USERNAME);
+        username = standardizePhoneNumber(context.getSession(), username);
+        UserModel user;
+        inputData.put(Details.USERNAME, Collections.singletonList(username));
+        UserProfileProvider profileProvider = context.getSession().getProvider(UserProfileProvider.class);
+        UserProfile profile = profileProvider.create(UserProfileContext.REGISTRATION_USER_CREATION, inputData);
+        user = profile.create();
+        user.setEnabled(true);
+        user.setAttribute("phoneNumber", Collections.singletonList(username));
+        createCredential(context, username, user);
+        return user;
+    }
+
+    private String standardizePhoneNumber(KeycloakSession session, String phoneNumber) {
         phoneNumber = phoneNumber.trim();
         String defaultCountryCode = CountryCodes.getCode(Utils.defaultRegion(session));
-        if (phoneNumber.startsWith(defaultCountryCode)) {
+        if (phoneNumber.startsWith("0")) {
+            return defaultCountryCode + phoneNumber.substring(1);
+        } else if (phoneNumber.startsWith(defaultCountryCode)) {
             return phoneNumber;
         } else {
             return defaultCountryCode + phoneNumber;
         }
     }
 
-    private boolean validateUser(AuthenticationFlowContext context, UserModel user, MultivaluedMap<String, String> inputData) {
-        if (!enabledUser(context, user)) {
-            return false;
-        }
-        String rememberMe = inputData.getFirst("rememberMe");
-        boolean remember = rememberMe != null && rememberMe.equalsIgnoreCase("on");
-        if (remember) {
-
-            context.getAuthenticationSession().setAuthNote(Details.REMEMBER_ME, "true");
-            context.getEvent().detail(Details.REMEMBER_ME, "true");
-        } else {
-            context.getAuthenticationSession().removeAuthNote(Details.REMEMBER_ME);
-        }
-        context.setUser(user);
-        return true;
-    }
-
+    // Copy from UsernameForm
     @Override
     protected Response challenge(AuthenticationFlowContext context, MultivaluedMap<String, String> formData) {
+        removeCookieSmsOtpAnswered(context);
         LoginFormsProvider forms = context.form();
-
         if (!formData.isEmpty()) forms.setFormData(formData);
-
         return forms.createLoginUsername();
     }
 
+    private void removeCookieSmsOtpAnswered(AuthenticationFlowContext context) {
+        URI uri = context.getUriInfo()
+                .getBaseUriBuilder()
+                .path("realms")
+                .path(context.getRealm().getName())
+                .build();
+        new SmsOtpMfaAuthenticator().
+                addCookie(context, "SMS_OTP_ANSWERED", null,
+                        uri.getRawPath(),
+                        null, null,
+                        0,
+                        false, true);
+    }
+
+    // Copy from UsernameForm
     @Override
     protected Response createLoginForm(LoginFormsProvider form) {
         return form.createLoginUsername();
@@ -147,8 +160,6 @@ public class PhoneLoginAndAutoCreateUserForm extends UsernamePasswordForm {
 
     @Override
     protected String getDefaultChallengeMessage(AuthenticationFlowContext context) {
-        if (context.getRealm().isLoginWithEmailAllowed())
-            return Messages.INVALID_USERNAME_OR_EMAIL;
         return Messages.INVALID_USERNAME;
     }
 }
--- keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/providers/spi/impl/DefaultPhoneProvider.java
+++ keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/providers/spi/impl/DefaultPhoneProvider.java
@@ -49,7 +49,7 @@ public class DefaultPhoneProvider implements PhoneProvider {
                     this.service + "' will be used. You can use keycloak start param '--spi-phone-default-service' to specify a different one. ");
 
         this.tokenExpiresIn = config.getInt("tokenExpiresIn", 60);
-        this.targetHourMaximum = config.getInt("targetHourMaximum", 60);
+        this.targetHourMaximum = config.getInt("targetHourMaximum", 20);
         this.sourceHourMaximum = config.getInt("sourceHourMaximum", 10);
     }
 
@@ -130,8 +130,6 @@ public class DefaultPhoneProvider implements PhoneProvider {
         }
 
         TokenCodeRepresentation token = TokenCodeRepresentation.forPhoneNumber(phoneNumber);
-
-        getTokenCodeService().persistCode(token, type, tokenExpiresIn);
 
         try {
             session.getProvider(MessageSenderService.class, service).sendSmsMessage(type, phoneNumber, token.getCode(), tokenExpiresIn, kind);
